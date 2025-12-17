24ca00802b68f71553136e0da93fbd8fc6a022b4
GezimC <gezim.ciriku@keds-energy.com>
2025-12-17 10:26:25 +0100

    initial author service

--- src/main/java/org/example/service/AuthorService.java
+++ src/main/java/org/example/service/AuthorService.java
@@ -1,4 +1,26 @@
 package org.example.service;
 
+import org.example.dao.AuthorDao;
+import org.example.model.Author;
+
 public class AuthorService {
+
+    private AuthorDao authorDao = new AuthorDao();
+
+    public boolean createAuthor(String name, String lastname)
+    {
+
+        if(name.isEmpty())
+        {
+            System.out.println("Name should not be empty");
+            return false;
+        }
+
+        Author author = new Author(name, lastname);
+
+        authorDao.createAuthor(author);
+
+
+        return true;
+    }
 }
