953747eed4fb47f66690739c752022c56d5500a9
GezimC <gezim.ciriku@keds-energy.com>
2025-12-17 10:49:25 +0100

    create author from dao

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,6 +1,8 @@
 package org.example;
 
+import org.example.dao.AuthorDao;
 import org.example.helper.DbConnection;
+import org.example.model.Author;
 
 import java.sql.Connection;
 
@@ -12,11 +14,24 @@ public class App
 {
     public static void main( String[] args )
     {
-        try (Connection connection = DbConnection.connect()) {
-            System.out.println("Connected to the database!");
-        } catch (Exception e) {
-            System.out.println("Failed to connect to the database!");
-            e.printStackTrace();
+//        try (Connection connection = DbConnection.connect()) {
+//            System.out.println("Connected to the database!");
+//        } catch (Exception e) {
+//            System.out.println("Failed to connect to the database!");
+//            e.printStackTrace();
+//        }
+
+        AuthorDao authDao = new AuthorDao();
+
+        boolean result =authDao.createAuthor(new Author("testabc", "abctest"));
+
+        if (result)
+        {
+            System.out.println("Author created!");
+        }
+        else
+        {
+            System.out.println("Author not created!");
         }
     }
 }
